7cf4f7706ff89a78d7538e32dda7ad9259bd6955
tatsu8123 <tatsu19962016@gmail.com>
2021-02-20 23:36:47 +0900

    signup

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oursite',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+	
+AUTH_USER_MODEL = 'oursite.StudentUser'
--- oursite/oursite/urls.py
+++ oursite/oursite/urls.py
@@ -14,14 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import base
+from django.contrib.auth import views as auth_views
 from . import test
 from . import timetable
 from . import circle
+from .views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', test.test, name='test'),
     path('timetable/', timetable.timetable, name='timetable'),
     path('circle/', circle.circle, name='circle'),
+    path('signup/',signup,name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
+    path('profile/', base.RedirectView.as_view(pattern_name="index")),
 ]
