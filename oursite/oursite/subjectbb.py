c573e14806d8e1c1a410647724ca583eeb483651
ehara2021 <s.e.job.2020@gmail.com>
2021-02-20 16:41:48 +0900

    授業情報掲示板追加

--- oursite/oursite/subjectbb.py
+++ oursite/oursite/subjectbb.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def subjectbb(request):
     return render(request, 'subjectBB.html')
+
+def subjectReview(request):
+    return render(request, 'subject_review.html')
